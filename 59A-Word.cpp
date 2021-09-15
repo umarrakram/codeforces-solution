7da96b72a38b7c176416d89a0a8259bec46d833f
Umar Akram <70350465+umarrakram@users.noreply.github.com>
2021-09-15 05:39:31 +0200

    Update 59A-Word.cpp
    You need to count the uppercase characters of the string by using "isupper()" and "islower()" predefined functions. Rather than using another for loop we can just use transform algorithm to either uppercase or lowercase the whole string.

--- 59A-Word.cpp
+++ 59A-Word.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-//    freopen("input.txt", "r",stdin);
-//    freopen("output.txt", "w",stdout);
+/*   freopen("input.txt", "r",stdin);
+    freopen("output.txt", "w",stdout); */
 
     string s;
     cin >> s;

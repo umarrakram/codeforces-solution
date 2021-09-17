1afa7360325a9984fe85c6e1816a0971b3509379
Umar Akram <70350465+umarrakram@users.noreply.github.com>
2021-09-17 09:30:57 +0200

    Create 71A- Way Too Long Words.cpp

---
+++ 71A- Way Too Long Words.cpp
@@ -0,0 +1,23 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main() {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        int t;
+        string s;
+     
+        cin >> t;
+        while (t--) {
+            cin >> s;
+            if (s.size() > 10) {
+                cout << s[0] << s.size() - 2 << s[s.size() - 1] << "\n";
+            } else if (s.size() <= 10) {
+                cout << s << "\n";
+            }
+     
+        }
+     
+     
+        return 0;
+    }
